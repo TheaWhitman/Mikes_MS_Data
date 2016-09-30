db0cf68993f08b8be4911225d5891136e84c8836
michaeljbraus <brausm@gmail.com>
2016-09-30 10:05:57 -0500

    update biom rank parsing

--- QIIME2Phyloseq.R
+++ QIIME2Phyloseq.R
@@ -9,13 +9,12 @@ biom <- import_biom("OTU_table.biom", parseFunction=parse_taxonomy_greengenes); 
 sam.data <- read.csv(file="sample.data.csv", row.names=1, header=TRUE)
 head(sam.data)
 sample_data(biom) <- sam.data 
-biom; head(otu_table(biom))
+biom
+head(otu_table(biom))
 # Normalize by relative abundance. 
 biom.relabund <- transform_sample_counts(biom, function(x) x / sum(x))
 ordNMDS <- ordinate(biom.relabund, method="NMDS", distance="bray")
-# Run 20 stress 0.1164499 
 ordNMDS.k3 <- ordinate(biom.relabund, method="NMDS", distance="bray", k=3)
-# Run 20 stress 0.08334116 
 ord.k3 <- plot_ordination(biom.relabund, ordNMDS.k3, shape="SampleSite", color="Date", label = "Date") + 
   geom_point(size=4) + 
   facet_wrap(~SampleSite) + 
