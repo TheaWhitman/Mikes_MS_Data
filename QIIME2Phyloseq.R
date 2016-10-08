d13a1555ed961825c901c115a8dbfc579f0d35d7
michaeljbraus <brausm@gmail.com>
2016-10-08 15:01:57 -0500

    switch to knitr

--- QIIME2Phyloseq.R
+++ QIIME2Phyloseq.R
@@ -3,25 +3,27 @@
 library(phyloseq)
 library(ggplot2)
 library(dplyr)
+library(Rmisc)
 setwd("~/Dropbox/clado-manuscript/Mikes_MS_Data/")
 # Load biom file. 
 biom <- import_biom("OTU_table.biom", "~/Dropbox/clado-manuscript/Nephele/PipelineResults_NMEPINZ20QK1/nephele_outputs/tree.tre", parseFunction=parse_taxonomy_greengenes)
 sam.data <- read.csv(file="sample.data.csv", row.names=1, header=TRUE)
 head(sam.data)
+sam.data$Date <- as.factor(sam.data$Date)
 sam.data$DateSite <- paste(sam.data$Date, sam.data$Site)
-head(sam.data)
-sample_data(biom) <- sam.data 
+head(sam.data); str(sam.data)
+sample_data(biom) <- sam.data
 biom; sample_data(biom)
 head(otu_table(biom))
 # Custom plotting. 
 nolegend <- theme(legend.position="none")
 readabund <- labs(y="read abundance")
 # Normalize by relative abundance. 
+#pdf(file="figs/ord.k3.pdf", height=7, width=9)
 biom.relabund <- transform_sample_counts(biom, function(x) x / sum(x))
 ordNMDS <- ordinate(biom.relabund, method="NMDS", distance="bray")
 ordNMDS.k3 <- ordinate(biom.relabund, method="NMDS", distance="bray", k=3)
-ord.k3 <- plot_ordination(biom.relabund, ordNMDS.k3, label="Date", shape="Site") + geom_point(size=2, color = "black") + geom_polygon(aes(fill=DateSite), alpha=0.6) + labs(title = "Cladophora, 2014") + theme_bw()
-#pdf(file="figs/ord.k3.pdf", height=7, width=9)
+ord.k3 <- plot_ordination(biom.relabund, ordNMDS.k3, shape="Site", color = "DateSite") + geom_point(size=2) + geom_polygon(aes(fill=DateSite), alpha=0.7) + labs(title = "Cladophora, 2014") + theme_bw()
 ord.k3
 #dev.off()
 # Facet by Date. 
@@ -53,16 +55,26 @@ relabund.methanos.genus <- relabund.methanos%>%
   distinct(Sample, GenusAbundance, TreatmentGroup, Site, Date, Family, Genus)
 head(relabund.methanos.genus)
 # 
-p <- ggplot(relabund.methanos.genus, aes(Date, GenusAbundance, color = Site))
-p <- p + geom_boxplot() + facet_wrap(~Genus, scales="free_y")
+p <- ggplot(relabund.methanos.genus, aes(as.factor(Date), GenusAbundance, color = Site))
+p <- p + geom_point() + facet_wrap(~Genus, scales="free_y")
 #pdf(file="figs/methanos.relabund.pdf", height=8, width=15)
 p
 #dev.off()
+
+# Means with error bars
+stats <- summarySE(biom, measurevar="Abundance", groupvars=c("bacsp","media")); stats
+p.stats <- ggplot(stats, aes(x = media, y = percloss, fill = bacsp))
+p.stats + geom_bar(stat = "identity", position=position_dodge(.9)) + geom_errorbar(aes(ymin=percloss-se, ymax=percloss+se), width=.2, colour="darkblue", position=position_dodge(.9)) + geom_rug()  + scale_fill_grey()
+
 # Plot richness. 
 #pdf(file="figs/richness.pdf", height=8, width=20)
 biom.rich <- plot_richness(biom, x="Date", color="Site")
 biom.rich
 #dev.off()
+
+### Shannon linear model test. 
+#biom.rich[1][1]
+
 # Stacked bar plots of methanotrophs. 
 rank_names(biom.relabund); sample_data(biom.relabund)
 #pdf(file="figs/barstack.methanos.pdf", height=8, width=12)
@@ -74,16 +86,17 @@ barstack.methanos
 network <- plot_net(biom, maxdist = 0.3, point_label = "SampleID.1", color = "Date", shape = "Site")
 network
 #dev.off()
-# Trees. 
+# Trees :)
 #pdf(file="figs/Methylococcaceae.tree.site.pdf", height=8, width=10)
 biom.relabund.Methylococcaceae <- subset_taxa(biom.relabund, Family %in% "Methylococcaceae")
+sample_data(biom.relabund.Methylococcaceae)$Date <- as.factor(sample_data(biom.relabund.Methylococcaceae)$Date)
 biom.relabund.Methylococcaceae
 head(tax_table(biom.relabund.Methylococcaceae))
-tree1 <- plot_tree(biom.relabund.Methylococcaceae, color="Site", shape="Site", label.tips="Genus")
+tree1 <- plot_tree(biom.relabund.Methylococcaceae, color="Date", shape="Date", label.tips="Genus", size = "Abundance")
 tree1
 #dev.off()
 #pdf(file="figs/Methylococcaceae.tree.datesite.pdf", height=8, width=10)
-tree2 <- plot_tree(biom.relabund.Methylococcaceae, color="DateSite", label.tips = "Genus")
+tree2 <- plot_tree(biom.relabund.Methylococcaceae, color="DateSite", label.tips = "Genus", size = "Abundance")
 tree2
 #dev.off()
 
